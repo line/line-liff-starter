10a440d9c4df22a35d47f9da6c24ba089b8e90a5
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2020-06-15 15:48:46 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -28,11 +28,7 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
             type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
+            text: "Hello World!"
         }]).then(function () {
             window.alert("Message sent");
         }).catch(function (error) {
